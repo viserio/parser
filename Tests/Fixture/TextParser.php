8eecd30ec0752218904753f61f36fb3566065a81
Daniel Bannert <prisis@users.noreply.github.com>
2018-05-17 15:02:55 +0200

    fixes #546 (#706)

--- Tests/Fixture/TextParser.php
+++ Tests/Fixture/TextParser.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-namespace Viserio\Component\Parser\Tests\Fixtures;
+namespace Viserio\Component\Parser\Tests\Fixture;
 
 use Viserio\Component\Contract\Parser\Parser as ParserContract;
 
